13cd542d0eb975ba018e91aa99aa8e955d1c6a6b
nonki7777 <nonki7777@gmail.com>
2013-09-09 16:30:24 -0600

    Update test code
    Signed-off-by: nonki7777 <nonki7777@gmail.com>

--- tests/test_form_analyzer.py
+++ tests/test_form_analyzer.py
@@ -2,9 +2,22 @@
 
 from __future__ import print_function
 
+import os
 import unittest
 from mock import patch
-from fileupl import FormAnalyzer
+import tempfile
+from fileupl import FormAnalyzer, up_limit
+
+
+def open_temp_file(size=128):
+    """Makes a temporary file with specified size and returns a handle.
+    Note: the temporary files will NOT be deleted automatically.
+    """
+    fh = tempfile.NamedTemporaryFile(delete=False)
+    fname = fh.name
+    fh.write('a' * size)
+    fh.close()
+    return open(fname, 'rb')
 
 
 class TestFormAnalyzer(unittest.TestCase):
@@ -21,8 +34,29 @@ class TestFormAnalyzer(unittest.TestCase):
             self.assertTrue(not f)
 
     def test_do_upload(self):
-        pass
-    
+
+        def file_maxsize(size):
+            wb = open_temp_file()
+            fpath = wb.name
+            wb.close()
+            os.remove(wb.name)
+            uplb = open_temp_file(size)
+            reachmax = fa.do_upload(uplb, fpath)
+            uplb.close()
+            os.remove(uplb.name)
+            os.remove(fpath)
+            return reachmax
+
+        fa = FormAnalyzer()
+        self.assertRaises(TypeError, lambda: fa.do_upload(None, None))
+            # 2nd arg must be str
+
+        self.assertFalse(file_maxsize(0))
+        self.assertFalse(file_maxsize(3))
+        self.assertFalse(file_maxsize(1024 + 1))
+        self.assertFalse(file_maxsize(1024 * (up_limit - 1)))
+        self.assertTrue(file_maxsize(1024 * (up_limit - 1) + 1))
+        
 
 if __name__ == "__main__":
     unittest.main()
